b9e12620b41aa740debab8a01366400affbf1d17
Xander <XanderRaaen@proton.me>
2026-04-14 20:51:03 +0200

    Cherrypick changes from api branch (#29)
    * Remove compilerOption causing issues.
    * Created error handling for missing env.

--- server_src/secret_handling.ts
+++ server_src/secret_handling.ts
@@ -1,10 +1,44 @@
+// --- Import the LogTape config --------------------
+import "./logtape_config.ts";
+import { getLogger } from "@logtape/logtape";
+const logger = getLogger(["server-backend"]);
+// --------------------------------------------------
+
 // Note that this package is experimental!
 // https://docs.deno.com/runtime/reference/env_variables/
 import { load } from "jsr:@std/dotenv";
 
-export const env = await load({
-  // Optional: choose a specific path (defaults to ".env")
-  envPath: ".env",
-  // Optional: also export to the process environment (so Deno.env can read it)
-  // export: true,
-});
+/**
+ * @returns Validated record of enviorment variables.
+ */
+export async function initializeEnvVars() {
+  const rawEnv = await load({
+    // Optional: choose a specific path (defaults to ".env")
+    envPath: ".env",
+    // Optional: also export to the process environment (so Deno.env can read it)
+    // export: true,
+  });
+
+  // Check that the required environment variable exist in imported environment variables.
+  const requiredEnvVars: string[] = [
+    "JWT_SERVER_SECRET",
+    "ADMIN_USER_PASSWORD",
+  ];
+  requiredEnvVars.forEach((env) => {
+    if (!(env in rawEnv)) {
+      const logMsg = `Envirorment variable: ${env} not found.`;
+      logger.fatal(logMsg);
+      throw new Error(logMsg);
+    }
+
+    if (!rawEnv[env]) {
+      const logMsg = `Envirorment variable: ${env} not populated.`;
+      logger.fatal(logMsg);
+      throw new Error(logMsg);
+    }
+  });
+
+  return rawEnv;
+}
+
+export const env = await initializeEnvVars();
