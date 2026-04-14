6a83e85a9f74bc6f4cc27d2939f7c5e9223467ef
Kristoffer Hartvig <hartvigkristoffer@gmail.com>
2026-04-14 10:26:33 +0200

    changed svg to icon from react icons

--- client_src/components/NavBar.tsx
+++ client_src/components/NavBar.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./NavBar.css";
+import {FaUser} from "react-icons/fa";
 
 function NavBar() {
   return (
@@ -20,20 +21,7 @@ function NavBar() {
           {/* User Icon SVG */}
           <div className="avatar">
             <a href="#">
-              <svg
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="black"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <circle cx="12" cy="8" r="4" />
-
-                <path d="M4 20c0-4 4-6 8-6s8 2 8 6" />
-              </svg>
+              <FaUser />
             </a>
           </div>
           <span className="username">Navn</span>
