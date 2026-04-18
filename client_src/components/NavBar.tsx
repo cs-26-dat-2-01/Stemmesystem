eca9becbd929a384135749d9bad308acf730eff1
Enes Toplica <enestoplica@MacBook-Pro-tilhrende-Enes-2.local>
2026-04-18 15:23:32 +0200

    feat: implement overview page

---
+++ client_src/components/NavBar.tsx
@@ -0,0 +1,32 @@
+import "./NavBar.css";
+import {FaUser} from "react-icons/fa";
+
+function NavBar() {
+  return (
+    <header className="navbar">
+      <div className="nav-left">
+        <div className="nav-logo">LOGO</div>
+        <span className="title">Se Afstemning</span>
+      </div>
+
+      <div className="nav-right-group">
+        <div className="nav-center">
+          <a href="#">Hjem</a>
+        </div>
+
+        <div className="vertical-divider"></div>
+
+        <div className="nav-right">
+          <div className="avatar">
+            <a href="#">
+              <FaUser />
+            </a>
+          </div>
+          <span className="username">Navn</span>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default NavBar;
