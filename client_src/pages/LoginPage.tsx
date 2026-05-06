63a507bb20944deed241146a98a6ddc2756e8613
Kristoffer Hartvig <hartvigkristoffer@gmail.com>
2026-05-06 14:49:22 +0200

    fix: prevent login page stylesheet affecting other pages

--- client_src/pages/LoginPage.tsx
+++ client_src/pages/LoginPage.tsx
@@ -48,43 +48,45 @@ function LoginPage({ setIsLoggedIn }: LoginPageProps) {
 
   return (
     <>
-      <div className="login-container">
-        <div className="form">
-          <h1>Login</h1>
-          {error && (
-            <p className="error-text">Forkert brugernavn eller adgangskode</p>
-          )}
-          <form onSubmit={handleLogin}>
-            <div className="input-wrapper">
-              <FaUser className="input-icon" />
-              <input
-                name="username"
-                id="username"
-                type="text"
-                autoComplete="username"
-                placeholder="brugernavn"
-                className={`input-field ${error ? "input-error" : ""}`}
-                required
-                onChange={handleUsernameChange}
-              />
-            </div>
-            <div className="input-wrapper">
-              <FaLock className="input-icon" />
-              <input
-                name="password"
-                id="password"
-                type="password"
-                autoComplete="current-password"
-                placeholder="adgangskode"
-                className={`input-field ${error ? "input-error" : ""}`}
-                required
-                onChange={handlePasswordChange}
-              />
-            </div>
-            <button type="submit" className="input-submit">
-              Login
-            </button>
-          </form>
+      <div className="login-page">
+        <div className="login-container">
+          <div className="form">
+            <h1>Login</h1>
+            {error && (
+              <p className="error-text">Forkert brugernavn eller adgangskode</p>
+            )}
+            <form onSubmit={handleLogin}>
+              <div className="input-wrapper">
+                <FaUser className="input-icon" />
+                <input
+                  name="username"
+                  id="username"
+                  type="text"
+                  autoComplete="username"
+                  placeholder="brugernavn"
+                  className={`input-field ${error ? "input-error" : ""}`}
+                  required
+                  onChange={handleUsernameChange}
+                />
+              </div>
+              <div className="input-wrapper">
+                <FaLock className="input-icon" />
+                <input
+                  name="password"
+                  id="password"
+                  type="password"
+                  autoComplete="current-password"
+                  placeholder="adgangskode"
+                  className={`input-field ${error ? "input-error" : ""}`}
+                  required
+                  onChange={handlePasswordChange}
+                />
+              </div>
+              <button type="submit" className="input-submit">
+                Login
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </>
