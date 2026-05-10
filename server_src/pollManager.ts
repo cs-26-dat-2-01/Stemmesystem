d838ba5f8bfd812270a59be6d70f1960133d909a
sofiek.s.larsen <WF18AA@student.aau.dk>
2026-05-10 23:20:16 +0200

    Tried to implement hash-chain
    (cherry picked from commit bf3c76fcf07b7d694774f1add360a13b3cceff6d)

--- server_src/pollManager.ts
+++ server_src/pollManager.ts
@@ -554,7 +554,7 @@ export class PollManager {
           ballotPrivacy: "secret",
           showTopN: poll.showTopN ?? 0,
           counts: countsWithText,
-          votes: votesResult.votes.map((v) => ({ uuid: v.id })),
+          votes: votesResult.votes.map((v) => ({ uuid: v.id, currentHash: v.currentHash})),
         },
         httpStatusCode: 200,
       };
@@ -567,6 +567,7 @@ export class PollManager {
         counts: countsWithText,
         votes: votesResult.votes.map((v) => ({
           uuid: v.id,
+          currentHash: v.currentHash,
           optionId: v.pollOptionId,
           optionText: optionTextById.get(v.pollOptionId) ?? "(unknown)",
         })),
